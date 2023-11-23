62fe7c903934ff0f589c3d60defb752876061c75
FrandenilsonAntony <iabyantony88@gmail.com>
2023-11-22 23:00:22 -0300

    "Feat: Create `routes` and config `start` in `Package.json`"

---
+++ src/server/routes/index.ts
@@ -0,0 +1,17 @@
+import {Router} from 'express';
+
+const router = Router();
+
+
+router.get('/', (_, res) => {
+   
+  return res.send('Olá, Dev'); 
+});
+
+router.post('/teste', (req, res) => {
+  console.log(req.body);
+
+  return res.json(req.body); 
+});
+
+export {router};
