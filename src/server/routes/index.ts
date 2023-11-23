a12492a23be34d7c470b54f815edb0766ef0937f
FrandenilsonAntony <iabyantony88@gmail.com>
2023-11-23 00:24:24 -0300

    "Feat: Install `httpStatusCodes`"

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import {StatusCodes} from 'http-status-codes';
 
 const router = Router();
 
@@ -11,7 +12,7 @@ router.get('/', (_, res) => {
 router.post('/teste', (req, res) => {
   console.log(req.body);
 
-  return res.json(req.body); 
+  return res.status(StatusCodes.ACCEPTED).json(req.body); 
 });
 
 export {router};
